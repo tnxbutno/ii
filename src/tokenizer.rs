97eb841197fd4c9fa49c20e0642a6b23af70dcb5
Valery <tehbrut@gmail.com>
2023-05-22 20:45:21 +0400

    feat: basic tokenizer

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -2,7 +2,32 @@ pub struct Tokenizer {}
 
 impl Tokenizer {
     /* Converts text to vector of tokens. Tokens are either letter or number */
-    pub fn tokenize(text: &str) -> Vec<&str> {
-        todo!()
+    pub fn tokenize(text: &str) -> Vec<String> {
+        let clean_string = text
+            .chars()
+            .filter(|c| !c.is_ascii_punctuation())
+            .collect::<String>();
+        clean_string
+            .split_whitespace()
+            .map(str::to_string)
+            .collect()
+    }
+}
+
+#[cfg(test)]
+mod tokenizer_tests {
+    use crate::tokenizer::Tokenizer;
+
+    #[test]
+    fn test_tokenize() {
+        let text = "Hello #{$}! I'm test suite & I ... contain number 32!!";
+        let res = Tokenizer::tokenize(&text);
+        let expected = vec![
+            "Hello", "I'm", "test", "suite", "I", "contain", "number", "32",
+        ];
+        assert!(
+            res.len() != expected.len() || res != expected,
+            "tokenization failed"
+        )
     }
 }
