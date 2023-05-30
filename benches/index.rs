9fb76fbeb330a2293f26735005e13d863766935c
Valery <tehbrut@gmail.com>
2023-05-30 12:05:08 +0400

    feat: add benchmark for search in index

--- benches/index.rs
+++ benches/index.rs
@@ -1,9 +1,9 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion, BenchmarkId};
 use inverted_index::index::{Document, InvertedIndex};
 
 const TEXT: &str = include_str!("war_and_peace.txt");
 
-pub fn index_benchmark(c: &mut Criterion) {
+pub fn add_index_benchmark(c: &mut Criterion) {
     let docs = TEXT
         .lines()
         .filter(|l| !l.is_empty())
@@ -22,5 +22,26 @@ pub fn index_benchmark(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, index_benchmark);
+pub fn search_index_benchmark(c: &mut Criterion) {
+    let docs = TEXT
+        .lines()
+        .filter(|l| !l.is_empty())
+        .enumerate()
+        .map(|(index, line)| Document {
+            id: index as u64,
+            text: line.to_string(),
+        })
+        .collect::<Vec<Document>>();
+
+    let mut index = InvertedIndex::default();
+    index.add(&docs);
+
+    c.bench_function("search-in-index", |b| {
+        b.iter(|| {
+            index.search("make peace")
+        })
+    });
+}
+
+criterion_group!(benches, add_index_benchmark, search_index_benchmark);
 criterion_main!(benches);
